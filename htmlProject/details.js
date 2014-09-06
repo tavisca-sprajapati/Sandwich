7dcf90852f001671c7a9b0cc551b0bb57c97599f
satishprajapati <sprajapati@tavisca.com>
2014-09-06 18:47:27 +0530

    modification in details page GUI

--- htmlProject/details.js
+++ htmlProject/details.js
@@ -1,9 +1,36 @@
-  
-$(document).ready(function(){
+  $(document).ready(function(){
   $("#flip").click(function(){
     $("#panel").slideToggle("slow");
   });
+
+  $("#showMe").click(function(){
+
+    $("#lab").animate({width:'300px',height:'550px'});
+    $("#mainpage").attr("style","overflow-y:auto;width:280px;height:520px;");
+    $("#ShowLess").show();
+    $("#ShowLess").html("<center><font size='3' color=white>Hide</font></center>");
+    $("#showMe").hide();
+  });
+
+
+
+$("#ShowLess").click(function(){
+$("#lab").animate({width:'100px',height:'20px'});
+$("#mainpage").attr("style","overflow-y:auto;width:0px;height:0px;");
+$("#ShowLess").hide();
+$("#showMe").show();
+
 });
+
+function moveup()
+{
+ alert("up"); 
+}
+});
+
+
+
+
 
 
 /*
