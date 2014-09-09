a92074dece08940630201d96ab1aef8d6a2277c1
parulgupta12 <pgupta@tavisca.com>
2014-09-09 14:34:08 +0530

    Update dealregistration.js

--- htmlProject/dealregistration.js
+++ htmlProject/dealregistration.js
@@ -19,32 +19,62 @@ function dealregistrationValidation()
 
     if(selected_value==0)
     {
-      alert("please select a Category!!!");
+      //alert("please select a Category!!!");
+      document.getElementById("1").innerHTML="Invalid choice";
       return false;
+    }
+    else
+    {
+       document.getElementById("1").innerHTML="";  
     }
     if(ck_title==""||ck_title==null)
     {
-      alert("Please enter some title!!!");
+      //alert("Please enter some title!!!");
+      document.getElementById("2").innerHTML="This field cannot be blank";
       return false;
+    }
+    else
+    {
+       document.getElementById("2").innerHTML="";   
     }
     if(ck_description==""||ck_description==null)
     {
-      alert("Please provide some description!!!");
+      //alert("Please provide some description!!!");
+      document.getElementById("3").innerHTML="This field cannot be blank";
       return false;
+    }
+    else
+    {
+      document.getElementById("3").innerHTML="";
     }
     if (!contact_name.match(ck_contactname)) 
     {
-      alert("Please enter a valid contact Name!!!");
+      //alert("Please enter a valid contact Name!!!");
+      document.getElementById("4").innerHTML="This field cannot be blank";
       return false;
+    }
+    else
+    {
+      document.getElementById("4").innerHTML="";
     }
     if(!ck_mail.test(mail_id))
     {
-      alert("Please enter a valid email id!!!");
+      //alert("Please enter a valid email id!!!");
+      document.getElementById("5").innerHTML="This field cannot be blank";
       return false;
+    }
+    else
+    {
+      document.getElementById("5").innerHTML="";
     }
     if(!ck_phone.test(phone_no))
     {
-      alert("Please enter a valid phone_no");
+      //alert("Please enter a valid phone_no");
+      document.getElementById("6").innerHTML="This field cannot be blank";
       return false;
+    }
+    else
+    {
+      document.getElementById("6").innerHTML="";
     }
 }
