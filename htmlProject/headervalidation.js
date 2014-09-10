45a3266566c78d00d2e72f3bd88f4da7b99c0b6a
tkatariya <tkatariya@tavisca.com>
2014-09-10 16:13:44 +0530

    header validations

--- htmlProject/headervalidation.js
+++ htmlProject/headervalidation.js
@@ -1,23 +1,53 @@
+var email_regex = /^([A-Za-z0-9_\-\.])+\@([A-Za-z0-9_\-\.])+\.([A-Za-z]{2,4})$/;
+var zipcode_regex=/^[0-9]{6}$/;
 
 
-       function checkEmail() {
+      function lemailValidation()
+      {
+        var email = $("#txtEmail").val();
+        if (!email_regex.test(email)) {
+        
+        $("#email_failed").html("Enter valid Email Id");
+        return true;
+        }
+        else {
+            $("#email_failed").html("");
+        }
+      }
 
-    var email = document.getElementById('txtEmail');
-    var filter = /^([a-zA-Z0-9_\.\-])+\@(([a-zA-Z0-9\-])+\.)+([a-zA-Z0-9]{2,4})+$/;
+    function lzipValidation()
+      {
+        var zip = $("#zip").val();
+        if (!zipcode_regex.test(zip)) {
+        
+        $("#zip_failed").html("Enter valid Zip code");
+        return true;
+        }
+        else {
+            $("#zip_failed").html("");
+        }
+      }
 
-    if (!filter.test(email.value)) {
-    alert('Please provide a valid email address');
-    email.focus;
-    return false;
- }
 
-  var zip = document.getElementById("zip").value;
-        var pattern = /^\d{6}?$/;
-        if (pattern.test(zip)) {
-            
-            return true;
-        } 
-            alert("It is not valid zip code !");
-            return false;
+function checkvalidation()
+{
+   
+    lemailValidation();
+    lzipValidation();
+    
+
+   
+
+    var flag = true;
+
+    if (lemailValidation()) {
+        flag = false;
+    }
+    if (lzipValidation()) {
+        flag = false;
+    }
+    
+    return flag;
 }
+
   
